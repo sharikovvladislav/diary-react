440a4d6731f720192e5b76219b2fbca49d4ec01d
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-09-19 19:37:50 +0300

    test prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+// test
 if (module.hot) {
-	module.hot.accept('./App', () => {
-		ReactDOM.render(<App />, document.getElementById('root'))
-	})
+  module.hot.accept("./App", () => {
+    ReactDOM.render(<App />, document.getElementById("root"));
+  });
 }
