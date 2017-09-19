651d3e30d99889405173f99fdcd0982411d9c15a
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-09-20 00:37:47 +0300

    add snapshot testing and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
+it('renders without crashing initial', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders without crashing shallow', () => {
+  expect(shallow(<App />)).toMatchSnapshot();
+});
+
+it('renders correctly full', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
