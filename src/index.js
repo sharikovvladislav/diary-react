62a89828ce53a28a61c662eb38bad0563d6cecfa
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-09-24 14:55:01 +0300

    create container/components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
+  module.hot.accept('./containers/App/App', () => {
     ReactDOM.render(<App />, document.getElementById('root'));
   });
 }
